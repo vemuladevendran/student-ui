796db8e1775ed3ec08422fc0f92130e84155094f
devendran <devendranvemula@gmail.com>
2021-07-30 21:50:48 +0530

    clean up

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -14,36 +14,34 @@ class Login extends PureComponent {
                     <div className="col-12 col-md-10">
                         <div className="row">
                             <div className="col-6 d-none d-lg-block">
-                                <img src="/assets/login-page-img.webp" alt="logo" className="w-100"></img>
+                                <img src="/assets/login-bg-2.jpg" alt="logo" className="w-100 h-100"></img>
                             </div>
-                            <div className="col-12 col-lg-6">
-                                <div className={`${ss.login_container} text-center rounded`}>
-                                    <h2 className="mt-2 text-white fw-bold">Login</h2>
+                            <div className={`${ss.login_container} col-12 col-lg-6 rounded text-center`}>
+                                <h2 className="mt-2 text-white fw-bold">Login</h2>
+                                <div className="mt-5">
                                     <div className="mt-5">
-                                        <div className="mt-5">
-                                            <span className="input-wrapper bg-white py-3 px-3 rounded">
-                                                <span><i className="bi bi-person-bounding-box me-1 text-warning"></i></span>
-                                                <input type="text" placeholder="Email" className={`${ss.login_input} border-0`}></input>
-                                            </span>
-                                        </div>
-                                        <div className="mt-5">
-                                            <span className="input-wrapper bg-white py-3 px-3 rounded">
-                                                <span><i className="bi bi-lock-fill me-1 text-warning"></i></span>
-                                                <input type="password" placeholder="Password" className={`${ss.login_input} border-0`}></input>
-                                            </span>
-                                        </div>
-                                        <div className="mt-5 d-flex justify-content-center">
-                                            <div className="d-flex justify-content-between" style={{ width: "280px" }}>
-                                                <div className="text-white">
-                                                    <input type="checkbox" className="me-2" id="remember"></input>
-                                                    <label htmlFor="remember">Remember me</label>
-                                                </div>
-                                                <a className="text-white" href="/">Forget Password</a>
+                                        <span className="input-wrapper bg-white py-3 px-3 rounded">
+                                            <span><i className="bi bi-person-bounding-box me-1 text-warning"></i></span>
+                                            <input type="text" placeholder="Email" className={`${ss.login_input} border-0`}></input>
+                                        </span>
+                                    </div>
+                                    <div className="mt-5">
+                                        <span className="input-wrapper bg-white py-3 px-3 rounded">
+                                            <span><i className="bi bi-lock-fill me-1 text-warning"></i></span>
+                                            <input type="password" placeholder="Password" className={`${ss.login_input} border-0`}></input>
+                                        </span>
+                                    </div>
+                                    <div className="mt-5 d-flex justify-content-center">
+                                        <div className="d-flex justify-content-between" style={{ width: "280px" }}>
+                                            <div className="text-white">
+                                                <input type="checkbox" className="me-2" id="remember"></input>
+                                                <label htmlFor="remember">Remember me</label>
                                             </div>
+                                            <a className="text-white" href="/">Forget Password</a>
                                         </div>
-                                        <div className="mt-5">
-                                            <button className="px-5 py-2 rounded bg-warning text-white fw-bold border-0">Login</button>
-                                        </div>
+                                    </div>
+                                    <div className="mt-5">
+                                        <button className="px-5 py-2 rounded bg-warning text-white fw-bold border-0">Login</button>
                                     </div>
                                 </div>
                             </div>
