4c464d87fe412065f9552b6d84e9cf0736ca0e61
devendran <devendranvemula@gmail.com>
2021-10-28 16:00:06 +0530

    new api url added

--- src/service/http/login.js
+++ src/service/http/login.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-
-const urlPort = "https://studentmanagmentdb.herokuapp.com/api/v1";
+const urlPort = "https://api.pecstudents.engineer/api/v1";
+// const urlPort = "https://studentmanagmentdb.herokuapp.com/api/v1";
 // const urlPort = 'http://localhost:5000/api/v1'
 
 export const login = (data) => {
--- src/service/http/user.js
+++ src/service/http/user.js
@@ -1,7 +1,9 @@
 
 import axios from "axios"
 
-const urlPort = 'https://studentmanagmentdb.herokuapp.com/api/v1'
+const urlPort = "https://api.pecstudents.engineer/api/v1";
+// const urlPort = "https://studentmanagmentdb.herokuapp.com/api/v1";
+// const urlPort = 'http://localhost:5000/api/v1'
 
 export const getUsers = () => {
     return (axios.get(`${urlPort}/users`));
